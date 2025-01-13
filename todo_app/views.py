dfe0d4e8176576b413cbd0df43575436d93277ee
Navnedia <87249556+Navnedia@users.noreply.github.com>
2025-01-12 21:12:10 -0600

    Created base template and add Bootstrap
    I defined a base template we can use for the shared page elements like the navbar. I've also included Bootstrap so pages that extend the base template can use these styles. I reference the Django documentation and this video as a starter: https://www.youtube.com/watch?v=nGIg40xs9e4

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 
-# Create your views here.
+# Define views here:
+
+def index(request: HttpRequest) -> HttpResponse:
+    return render(request, "index.html")
