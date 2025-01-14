aede3b28afa9b4e818f7faa5fae891560fa4ff07
acanady123 <canady.a@outlook.com>
2025-01-13 21:43:05 -0600

    Created the about page wireframe.

--- todo_app/views.py
+++ todo_app/views.py
@@ -5,3 +5,7 @@ from django.http import HttpRequest, HttpResponse
 
 def index(request: HttpRequest) -> HttpResponse:
     return render(request, "index.html")
+
+
+def about(request: HttpRequest) -> HttpResponse:
+    return render(request, "about.html")
