5140ab730d662f53e105dcdaf94ccd86b8473c58
afrozetk <afroze.tariq@gmail.com>
2025-01-15 00:44:16 -0600

    changed category to textarea

--- todo_app/forms.py
+++ todo_app/forms.py
@@ -10,13 +10,7 @@ class CreateForm(forms.ModelForm):
             'due_date': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'description': forms.Textarea(attrs={'class': 'form-control'}),
-            'category': forms.Select(attrs={'class': 'form-control'}),
+            'category': forms.Textarea(attrs={'class': 'form-control'}),
             'team': forms.Select(attrs={'class': 'form-control'})
         }
 
-
-            # def clean_due_date(self):
-            #     due_date = self.cleaned_data['due_date']
-            #     if due_date < timezone.now().date():  
-            #         raise ValidationError("The due date cannot be in the past.")
-            #     return due_date
