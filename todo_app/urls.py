bcdb37a34db2a7380a5be8689ae2bb6df9108928
afrozetk <afroze.tariq@gmail.com>
2025-01-15 00:44:40 -0600

    Update urls.py

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("new/", views.todo_create, name="create"),
+    path("todos/new/", views.todo_create, name="create"),
     path("about/", views.about, name="about"),
     path("todos/", views.todo_list, name="todo_list"),
     path("todos/<int:id>edit/", views.todo_edit, name="edit")
