0e8955d5ce09be4d1069d94b9ea8fb99b0728ec7
joedituri <172089200+joedituri@users.noreply.github.com>
2025-01-16 21:01:10 -0600

    FInished setting up docker

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = '8lu*6g0lg)9z!ba+a$ehk)xt)xxgb$i1&amp;022shmi1jcgihb*'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get("DEBUG", default=0))
